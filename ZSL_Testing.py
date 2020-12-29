8a3bcb23f700a1acf3113499b7e8ed429ffa7e21
Renwei Dian <1764857229@qq.com>
2020-12-29 16:59:59 +0800

    Update ZSL_Testing.py

--- ZSL_Testing.py
+++ ZSL_Testing.py
@@ -10,25 +10,18 @@ from model1 import *
 data2='pavia'
 PSF = fspecial('gaussian', 7, 3)
 downsample_factor=4
-[HRHSI,R]=dataset_input(data2,downsample_factor)
-HSI0=Gaussian_downsample(HRHSI,PSF,downsample_factor)
-SNRh=30;
-sigma = np.sqrt(np.sum(HSI0**2)/(10**(SNRh/10))/(HSI0.shape[0]*HSI0.shape[1]*HSI0.shape[2]));
-HSI0=HSI0+ 0*np.random.randn(HSI0.shape[0],HSI0.shape[1],HSI0.shape[2])
-MSI0=np.tensordot(R,  HRHSI, axes=([1], [0]))
-sigma = np.sqrt(np.sum(MSI0**2)/(10**(SNRh/10))/(MSI0.shape[0]*MSI0.shape[1]*MSI0.shape[2]));
-MSI0=MSI0+ 0*np.random.randn(MSI0.shape[0],MSI0.shape[1],MSI0.shape[2])
+[HSI,MSI,HRHSI]=dataset_input(data2,downsample_factor)
 
 p=10
-HSI3=HSI0.reshape(HSI0.shape[0],-1)
+HSI3=HSI.reshape(HSI.shape[0],-1)
 U0,S,V=np.linalg.svd(np.dot(HSI3,HSI3.T))
 U0=U0[:,0:int(p)]
-HSI0_Abun=np.tensordot(U0.T,  HSI0, axes=([1], [0]))
+HSI_Abun=np.tensordot(U0.T,  HSI, axes=([1], [0]))
 
-net2=CNN(p,MSI0.shape[0]).cuda() 
+net2=CNN(p,MSI.shape[0]).cuda() 
 net2.load_state_dict(torch.load(data2+'.pkl'))
-MSI_1=torch.Tensor(np.expand_dims(MSI0,axis=0))
-HSI_1=torch.Tensor(np.expand_dims(HSI0_Abun,axis=0))
+MSI_1=torch.Tensor(np.expand_dims(MSI,axis=0))
+HSI_1=torch.Tensor(np.expand_dims(HSI_Abun,axis=0))
 starttime = time.time()
 abudance = net2(HSI_1.cuda(),MSI_1.cuda())
 abudance=abudance.cpu().detach().numpy()
@@ -36,5 +29,9 @@ abudance1=np.squeeze(abudance)
 Fuse1=np.tensordot(U0, abudance1, axes=([1], [0]))
 endtime = time.time()
 print (endtime - starttime)
+a,b=metrics.rmse1(np.clip(Fuse1,0,1),HRHSI)
+print(a,b)
+Fuse1=np.clip(Fuse1,0,1)
+Fused_HSI=np.transpose(Fuse1,(1,2,0))
 
   
